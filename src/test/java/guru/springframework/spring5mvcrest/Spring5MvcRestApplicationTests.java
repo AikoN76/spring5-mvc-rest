0e9610e07e52bef8410a10533707c80b2927bbe1
Vincenzo <vincenzo.squillante@gmail.com>
2019-12-30 17:24:10 +0100

    Lesson 384: Using MapStruct.

--- src/test/java/guru/springframework/spring5mvcrest/Spring5MvcRestApplicationTests.java
+++ src/test/java/guru/springframework/spring5mvcrest/Spring5MvcRestApplicationTests.java
@@ -1,14 +1,12 @@
-package guru.springfamework.spring5mvcrest;
+package guru.springframework.spring5mvcrest;
 
-import guru.springframework.Spring5MvcRestApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes={Spring5MvcRestApplication.class})
+@SpringBootTest
 public class Spring5MvcRestApplicationTests {
 
 	@Test
