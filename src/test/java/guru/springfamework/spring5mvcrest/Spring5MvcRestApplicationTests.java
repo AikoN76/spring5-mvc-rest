bae550e695dd7f8907e9e7e856eca8116c370755
Vincenzo <vincenzo.squillante@gmail.com>
2019-12-30 15:24:15 +0100

    Lesson 382: Bootstrap with Spring Boot CommandLineRunner. Fix the main test

--- src/test/java/guru/springfamework/spring5mvcrest/Spring5MvcRestApplicationTests.java
+++ src/test/java/guru/springfamework/spring5mvcrest/Spring5MvcRestApplicationTests.java
@@ -1,12 +1,14 @@
 package guru.springfamework.spring5mvcrest;
 
+import guru.springframework.Spring5MvcRestApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes={Spring5MvcRestApplication.class})
 public class Spring5MvcRestApplicationTests {
 
 	@Test
