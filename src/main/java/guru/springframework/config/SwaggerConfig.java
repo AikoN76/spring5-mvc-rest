d3b4c7e56478332cbd9f5d0de57e7a8262bf058a
Vincenzo <vincenzo.squillante@gmail.com>
2020-01-01 19:16:05 +0100

    Lesson 405: Customizing Swagger with metadata.

--- src/main/java/guru/springframework/config/SwaggerConfig.java
+++ src/main/java/guru/springframework/config/SwaggerConfig.java
@@ -2,12 +2,15 @@ package guru.springframework.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.repository.NoRepositoryBean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2
@@ -20,7 +23,22 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metadata());
+    }
+
+    private ApiInfo metadata(){
+        Contact contact = new Contact("Vincenzo Squillante", "https://github.com/AikoN76", "vincenzo.squillante@gmail.com");
+
+        return new ApiInfo(
+          "Spring Framework Guru",
+                "Spring Framework 5: Beginner to Guru",
+                "1.0",
+                "Term of Service: use at your own risk :D",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
     }
 
 }
